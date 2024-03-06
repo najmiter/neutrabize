c534810e19ee4391fd8ad6cf309cfc245ab847ba
Najam <85332859+najmiter@users.noreply.github.com>
2024-03-06 18:13:25 +0500

    cleaning up

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,3 +1,6 @@
+const weather = document.getElementById("weather");
+const unit = document.getElementById("unit");
+
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const lati = position.coords.latitude;
@@ -8,24 +11,11 @@ if ("geolocation" in navigator) {
         fetch(WEATHER_API)
             .then((choosni) => choosni.json())
             .then((khbr) => {
-                const weather = document.getElementById("weather");
-                const unit = document.getElementById("unit");
                 weather.textContent = khbr.current.temperature_2m;
                 unit.textContent = khbr.current_units.temperature_2m;
             });
     });
 } else {
-    console.error("Geolocation is not supported by your browser");
+    weather.textContent = "";
+    console.error("Weather data could not be displayed");
 }
-
-// {
-//   latitude: 32,
-//   longitude: 73,
-//   generationtime_ms: 0.012993812561035156,
-//   utc_offset_seconds: 0,
-//   timezone: 'GMT',
-//   timezone_abbreviation: 'GMT',
-//   elevation: 190,
-//   current_units: { time: 'iso8601', interval: 'seconds', temperature_2m: '°C' },
-//   current: { time: '2024-03-06T12:45', interval: 900, temperature_2m: 19.4 }
-// }
