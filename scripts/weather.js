d67038d486ca118171d0d73e76cce7c37940d69c
Najam <85332859+najmiter@users.noreply.github.com>
2024-03-06 23:13:01 +0500

    let's not bother the weather people too much

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,30 +1,63 @@
 const weather = document.getElementById("weather");
 const unit = document.getElementById("unit");
+const weather_previously_fetched = localStorage.getItem("neutrabize_WEATHER");
 
-let weather_data_received = false;
+if (weather_previously_fetched) {
+    const { weather: old, at } = JSON.parse(weather_previously_fetched);
 
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        const lati = position.coords.latitude;
-        const long = position.coords.longitude;
-
-        const WEATHER_API = `https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${long}&current=temperature_2m`;
-
-        fetch(WEATHER_API)
-            .then((choosni) => choosni.json())
-            .then((khbr) => {
-                weather.textContent = khbr.current.temperature_2m;
-                unit.textContent = khbr.current_units.temperature_2m;
-                weather_data_received = true;
-            });
-    });
+    if (Date.now() > at + 1000 * 60 * 60) {
+        do_da_weather_thing();
+    } else {
+        weather.textContent = old.temp;
+        unit.textContent = old.unit;
+    }
 } else {
-    weather.textContent = "";
-    console.error("Weather data could not be displayed");
+    do_da_weather_thing();
 }
 
-setTimeout(() => {
-    if (!weather_data_received) {
+function do_da_weather_thing() {
+    let weather_data_received = false;
+
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const lati = position.coords.latitude;
+            const long = position.coords.longitude;
+
+            const WEATHER_API = `https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${long}&current=temperature_2m`;
+
+            fetch(WEATHER_API)
+                .then((choosni) => choosni.json())
+                .then((khbr) => {
+                    weather.textContent = khbr.current.temperature_2m;
+                    unit.textContent = khbr.current_units.temperature_2m;
+                    weather_data_received = true;
+
+                    localStorage.setItem(
+                        "neutrabize_WEATHER",
+                        JSON.stringify({
+                            at: Date.now(),
+                            weather: {
+                                temp: weather.textContent,
+                                unit: unit.textContent,
+                            },
+                        })
+                    );
+                });
+        });
+    } else {
         weather.textContent = "";
+        console.error("Weather data could not be displayed");
     }
-}, 3 * 1000);
+
+    setTimeout(() => {
+        if (!weather_data_received) {
+            if (weather_previously_fetched) {
+                const { weather: old } = JSON.parse(weather_previously_fetched);
+                weather.textContent = old.temp;
+                unit.textContent = old.unit;
+            } else {
+                weather.textContent = "";
+            }
+        }
+    }, 3 * 1000);
+}
