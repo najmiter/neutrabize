08a9c8c15025809e126ffaf2e1692f58c8a214fd
Najam <85332859+najmiter@users.noreply.github.com>
2024-03-07 10:54:08 +0500

    some weather things

--- scripts/weather.js
+++ scripts/weather.js
@@ -23,12 +23,12 @@ function do_da_weather_thing() {
             const lati = position.coords.latitude;
             const long = position.coords.longitude;
 
-            const WEATHER_API = `https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${long}&current=temperature_2m`;
+            const WEATHER_API = `https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${long}&current=temperature_2m,rain`;
 
             fetch(WEATHER_API)
                 .then((choosni) => choosni.json())
                 .then((khbr) => {
-                    weather.textContent = khbr.current.temperature_2m;
+                    weather.textContent = `${khbr.current.rain ? "🌧️" : "🌤️"} ${khbr.current.temperature_2m}`;
                     unit.textContent = khbr.current_units.temperature_2m;
                     weather_data_received = true;
 
