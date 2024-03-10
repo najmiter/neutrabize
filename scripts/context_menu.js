3d24c7880e85e25a63bf18efbf1181b3b968b242
Najam <85332859+najmiter@users.noreply.github.com>
2024-03-10 23:31:31 +0500

    keep the `contextmenu` in view

--- scripts/context_menu.js
+++ scripts/context_menu.js
@@ -1,9 +1,21 @@
 document.addEventListener("contextmenu", function (click) {
     click.preventDefault();
 
+    const menu_width = 270;
+    const menu_height = 230;
+
     menu.style.display = "flex";
-    menu.style.top = `${click.clientY}px`;
-    menu.style.left = `${click.clientX}px`;
+    menu.style.top = `${
+        click.clientY + menu_height > window.innerHeight
+            ? click.clientY - menu_height
+            : click.clientY
+    }px`;
+
+    menu.style.left = `${
+        click.clientX + menu_width > window.innerWidth
+            ? click.clientX - menu_width
+            : click.clientX
+    }px`;
 });
 
 document.addEventListener("click", hide_menu);
