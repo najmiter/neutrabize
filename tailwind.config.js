6ca212e90c47676bd0f8a31de6ca8677757fe4e0
Najam <realnajmiter@gmail.com>
2024-09-23 01:33:35 +0500

    live wallpaper in da house

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,ts}'],
+module.exports = {
+  content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
     extend: {},
   },
