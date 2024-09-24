7b094629c7e59d2ec07d1dad98dda4ca2b198d87
Najam <realnajmiter@gmail.com>
2024-09-25 00:27:37 +0500

    added top sites

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'get-in': {
+          from: {
+            opacity: 0,
+            transform: 'translateY(500px)',
+          },
+          to: {
+            opacity: 1,
+            transform: 'translateY(0px)',
+          },
+        },
+      },
+      animation: {
+        'get-in': 'get-in 600ms ease-in forwards',
+      },
+    },
   },
   plugins: [],
 };
